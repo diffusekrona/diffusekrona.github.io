26d6b4994c4ad120c8bb662123a7eecc6261b697
Nityanand Mathur <nityanandmathur@gmail.com>
2023-12-14 01:29:27 +0530

    init

---
+++ static/js/gallery.js
@@ -0,0 +1,22 @@
+function populateGallery(imageUrls, krona_or_lora ,fname, galleryId) {
+    var gallery = document.getElementById(galleryId);
+
+    imageUrls.forEach(function(image) {
+        var container = document.createElement('div');
+        container.className = 'image-container';
+        
+        var img = document.createElement('img');
+        img.src = "./static/" + krona_or_lora + '/' + fname + '/' + 'generated/' + image;
+        img.width = 224;
+        container.appendChild(img);
+
+        // Create a caption for each image
+        var caption = document.createElement('h6');
+        caption.textContent = image.replace('.png', ''); // Remove .png from the filename
+        caption.className = 'caption'; // Add the 'caption' class to the caption
+        caption.setAttribute('data-text', image.replace('.png', '')); // Set data-text attribute
+
+        container.appendChild(caption);
+        gallery.appendChild(container);
+    });
+}
